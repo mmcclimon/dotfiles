9f1bba7eb2907eadd209de71f5169f9e0a899b4d
Michael McClimon <mjmcclim@indiana.edu>
2014-04-29 09:16:15 -0400

    Fix path for work machine

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,8 +1,8 @@
 typeset -U path     # uniques
 
 # make sure right perl/ruby are here
-export PERLBREW_ROOT=~/.perl5/perlbrew
-source ~/.perl5/perlbrew/etc/bashrc
+export PERLBREW_ROOT=~/perl5/perlbrew
+source ~/perl5/perlbrew/etc/bashrc
 source ~/.rvm/scripts/rvm
 
 function prepend_to_path    { path=("$1" $path); }
@@ -10,9 +10,10 @@ function append_to_path     { path+=("$1"); }
 function prepend_to_manpath { manpath=("$1" $manpath); }
 function append_to_manpath  { manpath+=("$1"); }
 
+prepend_to_path /usr/local/bin
+prepend_to_path /usr/local/opt/coreutils/libexec/gnubin
 prepend_to_path ~/bin
-prepend_to_path /usr/local/texlive/2013/bin/i386-linux
 
-append_to_manpath /usr/local/texlive/2013/texmf-dist/doc/man
+prepend_to_manpath /usr/local/opt/coreutils/libexec/gnuman
 
 export PATH
