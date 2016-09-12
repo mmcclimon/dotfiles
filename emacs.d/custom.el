e648bb01b81389e721a6436b5901c84784d89151
Michael McClimon <michael@mcclimon.org>
2016-09-12 14:27:06 -0400

    Switch from ace-jump to avy

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -61,4 +61,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(avy-lead-face ((t (:background "goldenrod" :foreground "gray20"))))
+ '(avy-lead-face-0 ((t (:background "goldenrod" :foreground "gray20"))))
+ '(avy-lead-face-2 ((t (:background "goldenrod1" :foreground "gray12")))))
