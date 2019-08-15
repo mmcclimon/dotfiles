d641da4a5018a81c4f58d1115551e2e846372010
Michael McClimon <michael@mcclimon.org>
2019-08-14 22:23:20 -0400

    zsh: better history

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,13 +1,12 @@
-## Command history configuration
-if [ -z $HISTFILE ]; then
-    HISTFILE=$HOME/.zsh_history
-fi
-HISTSIZE=1000
-SAVEHIST=1000
+HISTFILE=$HOME/.zsh_history
+HISTSIZE=1000000
+SAVEHIST=1100000
 
 setopt append_history
+setopt inc_append_history
 setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
+setopt hist_reduce_blanks
 setopt hist_verify
