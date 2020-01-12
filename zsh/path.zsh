bb9c27c497879a196d019e2c9d7e036f8add0a91
Michael McClimon <michael@fastmailteam.com>
2020-01-12 14:56:20 -0500

    zsh: do not mangle path on remote systems

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -5,10 +5,6 @@ function append_to_path     { path+=("$1"); }
 function prepend_to_manpath { manpath=("$1" $manpath); }
 function append_to_manpath  { manpath+=("$1"); }
 
-prepend_to_path /usr/local/bin
-prepend_to_path /usr/local/sbin
-prepend_to_path $(brew --prefix)/opt/coreutils/libexec/gnubin
-prepend_to_manpath $(brew --prefix)/opt/coreutils/libexec/gnuman
 prepend_to_path ~/bin
 
 append_to_path /usr/local/texbin
@@ -17,6 +13,10 @@ append_to_path /usr/local/texbin
 # use historical settings on other macs
 if [ `hostname -s` = 'trane' ]
 then
+    prepend_to_path /usr/local/bin
+    prepend_to_path /usr/local/sbin
+    prepend_to_path $(brew --prefix)/opt/coreutils/libexec/gnubin
+    prepend_to_manpath $(brew --prefix)/opt/coreutils/libexec/gnuman
     eval "$(rbenv init -)"
     eval "$(plenv init - zsh)"
     eval "$(pyenv init -)"
