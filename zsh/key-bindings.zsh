a919b5e459dd63277c2fc8a3ff20dce526ec588d
Michael McClimon <michael@mcclimon.org>
2020-07-03 10:34:20 -0400

    zsh: tidy up keybindings file

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -1,50 +1,22 @@
-# (oh-my-zsh defaults)
-bindkey -e
-bindkey '\ew' kill-region
-bindkey -s '\el' "ls\n"
-bindkey '^r' history-incremental-search-backward
-bindkey "^[[5~" up-line-or-history
-bindkey "^[[6~" down-line-or-history
-
-# make search up and down work, so partially type and hit up/down to find relevant stuff
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
-
-bindkey ' ' magic-space    # also do history expansion on space
-
-bindkey "^[[1;5C" forward-word
-bindkey "^[[1;5D" backward-word
-
-bindkey '^[[Z' reverse-menu-complete
-
-# Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
-bindkey '^?' backward-delete-char
-bindkey "^[[3~" delete-char
-bindkey "^[3;5~" delete-char
-bindkey "\e[3~" delete-char
-
-#############################
-
 # Use vim cli mode, but with reasonable insert-mode shortcuts
 bindkey -v
-bindkey '^P' up-history
-bindkey '^N' down-history
+bindkey '^p' up-history
+bindkey '^n' down-history
 
-bindkey '^A' beginning-of-line
-bindkey '^E' end-of-line
-bindkey '^B' backward-char
-bindkey '^F' forward-char
+bindkey '^a' beginning-of-line
+bindkey '^e' end-of-line
+bindkey '^b' backward-char
+bindkey '^f' forward-char
 
 # backspace and ^h working even after
 # returning from command mode
 bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+bindkey '^d' delete-char
 
 bindkey '^b' backward-char
 
-# ctrl-w removed word backwards
-bindkey '^w' backward-kill-word
-
-# ctrl-r starts searching history backward
 bindkey '^r' history-incremental-search-backward
 
+bindkey '^t' gosmacs-transpose-chars
