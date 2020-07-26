1540d1b24c5f99eede2096a391926943c7193478
Michael McClimon <michael@mcclimon.org>
2020-07-26 19:25:36 -0400

    hs: handle built-in display, too

--- hammerspoon/resizer.lua
+++ hammerspoon/resizer.lua
@@ -1,5 +1,6 @@
 -- entirely so that I don't have to quote keys everywhere else.
 local screen_lookup = {
+  ["Color LCD"]   = "int",  -- really, macos?
   ["DELL P2715Q"] = "ext",
 }
 
@@ -8,9 +9,12 @@ local screen_config = {
     ext = function (frame)
       return { x = 140, y = 130, w = 925, h = 880 }
     end,
+    int = function (frame)
+      return { x = 130, y = 0, w = 925, h = frame.h }
+    end,
   },
   ["Slack"] = {
-    ext = function (frame)
+    all = function (frame)
       local left = frame.x + frame.w - 950
       return { x = left, y = 0, w = 950, h = 850 }
     end,
@@ -20,19 +24,23 @@ local screen_config = {
       local width = 3 * frame.w / 4;
       return { x = 0, y = 0, w = width, h = frame.h }
     end,
+    int = function (frame)
+      return { x = 0, y = 0, w = frame.w - 75, h = frame.h }
+    end,
   }
 };
 
 local function resizeWindows (app_name)
-  for app_name, cfg in pairs(screen_config) do
+  for app_name, app_config in pairs(screen_config) do
     local app = hs.application.find(app_name)
     local screen = screen_lookup[ hs.screen.primaryScreen():name() ]
+    local calc_size = app_config[screen] or app_config["all"]
 
-    if app and screen then
+    if app and screen and calc_size then
       local windows = app:visibleWindows()
       for _, window in ipairs(windows) do
         local frame = window:screen():fullFrame()
-        local geom = cfg[screen](frame)
+        local geom = calc_size(frame)
         window:move(geom)
         break -- one window is enough, in case of madness
       end
