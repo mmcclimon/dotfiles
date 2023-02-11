0ed65ffb5e566fc9bff7fa65fbb78fd408ebeaeb
Michael McClimon <michael@mcclimon.org>
2023-02-10 21:29:31 -0500

    path.zsh: rewrite to be generic
    i.e., remove all the dependencies on the hostname.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -4,38 +4,44 @@ function prepend_to_path    { path=("$1" $path); }
 function append_to_path     { path+=("$1"); }
 function prepend_to_manpath { manpath=("$1" $manpath); }
 function append_to_manpath  { manpath+=("$1"); }
+function command_exists     { command -v "$1" >/dev/null }
 
-# make sure right perl/ruby are here
-# use historical settings on other macs
-hostname=$(hostname -s)
-if [[ $hostname == 'trane' || $hostname == 'newk' ]]
-then
-    prepend_to_path /opt/local/bin
-    prepend_to_path /opt/local/sbin
-    prepend_to_path /opt/local/libexec/gnubin
+function maybe_prepend_to_path {
+  if [[ -d "$1" ]]; then
+    prepend_to_path "$1"
+  fi
+}
 
-    append_to_path "$HOME/.plenv/bin"
-    append_to_path "$HOME/.pyenv/bin"
-    prepend_to_path "$HOME/.cargo/bin"
-    # append_to_path "$HOME/.rakuenv/bin"
-    append_to_path "$HOME/.go/bin"
-
-    eval "$(plenv init - zsh)"
-    eval "$(direnv hook zsh)"
-    # eval "$(rakuenv init - zsh)"
-
-    # jeez pyenv is so slow; this is way faster
-    prepend_to_path "$HOME/.pyenv/shims"
-    export PYENV_SHELL=zsh
-    source '/Users/michael/.pyenv/libexec/../completions/pyenv.zsh'
+if [[ -x /opt/local/bin/port ]]; then
+  prepend_to_path /opt/local/bin
+  prepend_to_path /opt/local/sbin
+  prepend_to_path /opt/local/libexec/gnubin
 fi
-unset hostname
+
+maybe_prepend_to_path "$HOME/.plenv/bin"
+maybe_prepend_to_path "$HOME/.pyenv/bin"
+maybe_prepend_to_path "$HOME/.cargo/bin"
+maybe_prepend_to_path "$HOME/.go/bin"
+
+if command_exists plenv; then
+  eval "$(plenv init - zsh)"
+fi
+
+if command_exists pyenv; then
+  export PYENV_SHELL=zsh
+  prepend_to_path "$HOME/.pyenv/shims"
+  source '/Users/michael/.pyenv/libexec/../completions/pyenv.zsh'
+fi
+
+if command_exists direnv; then
+  eval "$(direnv hook zsh)"
+fi
 
 prepend_to_path ~/bin
 
-# Dunno about this, but let's try it!
-typeset -U cdpath
-cdpath=("$HOME/code/fm" $cdpath)
-
 export GOPATH="$HOME/.go"
-export PATH
+
+# I like this, but this is probably not useful any more, so.
+typeset -U cdpath
+# cdpath=("$HOME/code/fm" $cdpath)
+
