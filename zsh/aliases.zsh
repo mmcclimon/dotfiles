5bf1c310d24688583efd5af0b8507c580c55f492
Michael McClimon <michael@mcclimon.org>
2023-02-10 19:58:56 -0500

    aliases.zsh: use a function for dir hashing
    In service of making this more generic.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -27,7 +27,13 @@ alias gitn='git --no-pager'
 alias tls='tmux ls'
 
 # directory shortcuts
-hash -d txbe=~/code/fm/Topicbox-Backend
-hash -d txfe=~/code/fm/Topicbox-Frontend
-hash -d hm=~/code/fm/hm
-hash -d ix=~/code/fm/Ix
+function maybe_hash_dir {
+  [[ -d "$2" ]] && hash -d "$1"="$2"
+}
+
+maybe_hash_dir txbe ~/code/fm/Topicbox-Backend
+maybe_hash_dir txfe ~/code/fm/Topicbox-Frontend
+maybe_hash_dir hm ~/code/fm/hm
+maybe_hash_dir ix ~/code/fm/Ix
+
+unfunction maybe_hash_dir
